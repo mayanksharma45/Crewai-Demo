483e2db061d6786318ea6cb2a639048d5a59cb52
Mayank Sharma <156849549+mayanksharma45@users.noreply.github.com>
2025-06-18 22:42:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,4 @@
-from crewai import LLM
-import os
+from crewai_tools import SerperDevTool
 from dotenv import load_dotenv
-
-load_dotenv()
-
-llm = LLM(
-    provider="google",
-    model="gemini/gemini-2.0-flash",
-    api_key=os.getenv("GOOGLE_API_KEY")
-)
-
-print(llm.call("What is CrewAI and how does it work?"))
+from crewai import Agent, Task, Crew, LLM
+import os
